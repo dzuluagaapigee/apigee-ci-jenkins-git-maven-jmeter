1305babeab686a560d56535111a6ba3db75d1df5
Saul Zukauskas <saulius.zukauskas@gmail.com>
2014-04-24 20:13:14 +0100

    Housekeeping on my local repo

--- Vagrant/install-jenkins-plugins.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-java -jar ./jenkins/jenkins-cli.jar -s http://localhost:8088/ install-plugin performance -deploy
-java -jar ./jenkins/jenkins-cli.jar -s http://localhost:8088/ install-plugin github -deploy
-java -jar ./jenkins/jenkins-cli.jar -s http://localhost:8088/ install-plugin github-oauth -deploy
-java -jar ./jenkins/jenkins-cli.jar -s http://localhost:8080/ install-plugin jira -deploy
