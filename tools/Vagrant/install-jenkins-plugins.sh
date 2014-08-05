b146993f6f769851f549574caae4f181f7e1fecd
Diego Zuluaga <dzuluaga@apigee.com>
2014-08-04 20:31:42 -0400

    include email-ext plugin

--- tools/Vagrant/install-jenkins-plugins.sh
+++ tools/Vagrant/install-jenkins-plugins.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-java -jar ./jenkins/jenkins-cli.jar -s http://localhost:8088/ install-plugin performance -deploy
-java -jar ./jenkins/jenkins-cli.jar -s http://localhost:8088/ install-plugin github -deploy
-java -jar ./jenkins/jenkins-cli.jar -s http://localhost:8088/ install-plugin github-oauth -deploy
+java -jar ./jenkins/jenkins-cli.jar -s http://localhost:8080/ install-plugin performance -deploy
+java -jar ./jenkins/jenkins-cli.jar -s http://localhost:8080/ install-plugin github -deploy
+java -jar ./jenkins/jenkins-cli.jar -s http://localhost:8080/ install-plugin github-oauth -deploy
 java -jar ./jenkins/jenkins-cli.jar -s http://localhost:8080/ install-plugin jira -deploy
+java -jar ./jenkins/jenkins-cli.jar -s http://localhost:8080/ install-plugin ghprb -deploy
+java -jar ./jenkins/jenkins-cli.jar -s http://localhost:8080/ install-plugin email-ext -deploy
