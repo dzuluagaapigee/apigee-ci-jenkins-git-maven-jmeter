3ab9030a4224a4fb8bd33b04a6e1201a0e3c7a87
Diego Zuluaga <dzuluaga@apigee.com>
2014-08-06 16:25:29 -0400

    changes to bootstrap.sh

--- tools/Vagrant/bootstrap.sh
+++ tools/Vagrant/bootstrap.sh
@@ -9,3 +9,27 @@ sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources
 sudo apt-get update
 sudo apt-get install -y jenkins
 sudo apt-get install -y git
+sudo apt-get install zip -y unzip
+
+#install node.js
+# sudo apt-get update
+# sudo curl -sL https://deb.nodesource.com/setup | sudo bash -
+# sudo apt-get install -y nodejs
+
+#install maven
+# sudo apt-get update
+# sudo apt-get install -y maven
+
+#install npm
+# sudo apt-get update
+# sudo apt-get install -y npm
+
+#aglio
+# sudo apt-get update
+# sudo npm install -g aglio
+
+#grunt-cli
+# sudo npm install -g grunt-cli
+
+#dredd
+# sudo npm install dredd
