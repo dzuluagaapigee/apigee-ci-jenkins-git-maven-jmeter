a1fdf6c061e8b5196cbff956cc014145b8b21d46
dzuluaga <dzuluaga@apigee.com>
2015-03-13 18:34:08 -0400

    folder to explain how to recover vagrant boxes with files

---
+++ tools/Vagrant/apigee-kit-vm/bootstrap.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get install -y default-jre
+sudo apt-get install -y default-jdk
+
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install -y jenkins
+sudo apt-get install -y git
+sudo apt-get install zip -y unzip
+
+
+#install node.js
+sudo apt-get update
+sudo curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install -y nodejs
+
+#install maven
+# sudo apt-get update
+# sudo apt-get install -y maven
+
+#install npm
+sudo apt-get update
+sudo apt-get install -y npm
+
+#aglio
+# sudo apt-get update
+# sudo npm install -g aglio
+
+#grunt-cli
+sudo npm install -g grunt-cli
+
+#dredd
+# sudo npm install dredd
---
+++ tools/Vagrant/apigee-kit-vm/sharing-vagrant-boxes.md
@@ -0,0 +1,5 @@
+Sharing Vagrant Boxes
+=====
+A common requirement is to share Vagrant boxes with other developers without downloading images from the Internet. Intead developers can share a box using USB drives, local external storage that is faster than the Internet.
+
+#### Step 1: Install Virtual Box
