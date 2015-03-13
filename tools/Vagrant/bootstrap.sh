2b9187695bd9360c960618a67fe947e56762125b
dzuluaga <dzuluaga@apigee.com>
2015-03-13 15:27:45 -0400

    adding node and npm

--- tools/Vagrant/bootstrap.sh
+++ tools/Vagrant/bootstrap.sh
@@ -11,25 +11,26 @@ sudo apt-get install -y jenkins
 sudo apt-get install -y git
 sudo apt-get install zip -y unzip
 
+
 #install node.js
-# sudo apt-get update
-# sudo curl -sL https://deb.nodesource.com/setup | sudo bash -
-# sudo apt-get install -y nodejs
+sudo apt-get update
+sudo curl -sL https://deb.nodesource.com/setup | sudo bash -
+sudo apt-get install -y nodejs
 
 #install maven
 # sudo apt-get update
 # sudo apt-get install -y maven
 
 #install npm
-# sudo apt-get update
-# sudo apt-get install -y npm
+sudo apt-get update
+sudo apt-get install -y npm
 
 #aglio
 # sudo apt-get update
 # sudo npm install -g aglio
 
 #grunt-cli
-# sudo npm install -g grunt-cli
+sudo npm install -g grunt-cli
 
 #dredd
 # sudo npm install dredd
